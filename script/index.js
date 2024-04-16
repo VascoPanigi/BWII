afdd5955073a4a277df1e7374e79062259903247
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-04-16 14:45:28 +0200

    adds random cards - main section

--- script/index.js
+++ script/index.js
@@ -1,41 +1,12 @@
-// const url = 'https://deezerdevs-deezer.p.rapidapi.com/infos';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '0704e3122cmshbb05f15ef4f0571p1d5d41jsn006966e6ebea',
-// 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
-// 	}
-// };
+const getPlaylist = async () => {
+  const getRandomNumber = () => Math.floor(Math.random() * 500) + 1;
 
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
+  const id = getRandomNumber();
+  console.log(id);
+  // const id = 76;
 
-//PROCESSO:
-//Voglio fetchare l'URL e voglio popolare la pagina di elementi card playlist
-//TODO 1: fetchare il giusto API
-//TODO 2: trovare i giusti attributi dell'ogetto da inserire come elementi nella pagina
-//TODO 3: creare gli elementi nella pagina prima con innerHTML e in fase di rifinizione del
-//codice sostutuire con dei create-classlist-append
-//TODO 4: RANDOMIZZARE il fetch inserendo un math.random
-//TODO 5: non impazzire
+  const url = "https://deezerdevs-deezer.p.rapidapi.com/playlist/" + id;
 
-//TODO per giorno 2:
-//spostare la funzione di fetch e renderla autonoma
-//creare un fixed amount di card per pagina usando la funzione random
-//controllare IF uno dei parametri (tipo la PICTURE) della playlist è vuoto
-//questo significa che non c'è nessuna playlist in quell'API
-//pescare un nuovo numero finché la playlist che esce non è popolata
-
-const url = "https://deezerdevs-deezer.p.rapidapi.com/playlist/76";
-const row = document.getElementById("row");
-
-//QUESTO CODICE QUI SOTTO FUNZIONA
-window.addEventListener("DOMContentLoaded", async () => {
   const options = {
     method: "GET",
     body: JSON.stringify(),
@@ -50,50 +21,80 @@ window.addEventListener("DOMContentLoaded", async () => {
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    createNewCard(result);
+    return result;
   } catch (error) {
     console.error(error);
   }
-});
-
-//QUESTA sarà la base di partenza della funzione (basata su una versione precedente del codice)
-
-// const getPlaylist = async () => {
-//   const options = {
-//     method: "GET",
-//     body: JSON.stringify(),
-
-//     headers: {
-//       "Content-Type": "application/json",
-//       "X-RapidAPI-Key": "0704e3122cmshbb05f15ef4f0571p1d5d41jsn006966e6ebea",
-//       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-//     },
-//   };
-
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.json();
-//     console.log(result);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-const createNewCard = (playlist) => {
-  const row = document.getElementById("row");
-
-  const div = document.createElement("div");
-  div.classList.add("col-md-3");
-  div.innerHTML = `
-    <div class="card card-container ">
-    <div class='d-flex flex-column align-items-center'>
-    <img src="${playlist.picture_medium}" class="card-img-top m-2 " alt="playlist cover" />
-    </div>
-    <div class="card-body ms-2 me-2 mb-2 d-block flex-column align-items-start">
-      <h5 class="card-title text-white">${playlist.title}</h5>
-      <p class="card-text color-text">${playlist.creator.name}</p>
-    </div>
-  </div>
-    `;
-  row.appendChild(div);
 };
+
+console.log(getPlaylist());
+
+const createNewCard = async (playlist) => {
+  if (!playlist || !playlist.title || !playlist.picture_medium || !playlist.creator || !playlist.creator.name) {
+    console.log("Error: Invalid playlist data");
+    return;
+  }
+
+  const sequence = "https://e-cdns-images.dzcdn.net/images/cover//250x250-000000-80-0-0.jpg";
+  const regex = new RegExp(sequence);
+
+  if (regex.test(playlist.picture_medium)) {
+    console.log(playlist);
+
+    console.log("NO PICTURE");
+    return;
+  }
+
+  console.log(playlist);
+  const playlistName = playlist.title;
+  const playlistPicture = playlist.picture_medium;
+  const playlistCreator = playlist.creator.name;
+  const playlistEmptyValue = playlist.message;
+
+  if (playlistEmptyValue != "no data") {
+    const row = document.getElementById("row");
+
+    const div = document.createElement("div");
+    div.classList.add("col-md-3", "col-sm-4", "col-xxl-2");
+    div.innerHTML = `
+      <div class="card card-container playlist-card-container">
+      <div class='d-flex flex-column align-items-center'>
+      <img src="${playlistPicture}" class="card-img-top m-2 " alt="playlist cover" />
+      </div>
+      <div class="card-body ms-2 me-2 mb-2 d-block flex-column align-items-start">
+        <h5 class="card-title text-white">${playlistName}</h5>
+        <p class="card-text color-text">${playlistCreator}</p>
+      </div>
+    </div>
+      `;
+    row.appendChild(div);
+  } else {
+    getPlaylist();
+    createNewCard();
+  }
+};
+
+console.log(createNewCard());
+
+const generateHomepagePlaylistCards = async () => {
+  let cardElementsOnPage = document.querySelectorAll(".playlist-card-container");
+
+  while (cardElementsOnPage.length < 12) {
+    const playlist = await getPlaylist();
+    if (playlist) {
+      createNewCard(playlist);
+      cardElementsOnPage = document.querySelectorAll(".playlist-card-container");
+    } else {
+      console.log("Failed to fetch playlist data.");
+      break;
+    }
+  }
+};
+
+const init = async () => {
+  generateHomepagePlaylistCards();
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  init();
+});
