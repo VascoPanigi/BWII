4563c721dabe364867cd2402f247c51f36ea6335
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-04-17 22:47:31 +0200

    adds functions

--- script/search.js
+++ script/search.js
@@ -1,27 +1,27 @@
+// //RATIO > l-utente digita un nome-per esempio eminem
+
+// //2 io quindi metto questo parametro nell-endpoint
+// //https://deezerdevs-deezer.p.rapidapi.com/search?q={query}
+
+// //3- genero una card per l-artista
+// //4- faccio il display di 5 canzoni
+// //5- tempo permettendo - faccio vedere album e playlist in basso
+
 const form = document.querySelector("form");
 const inputField = document.getElementById("search-value");
+const top5SongsContainer = document.getElementById("top-5-songs");
+const artistCardContainer = document.querySelector(".artist-container");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault();
+
   const searchValue = inputField.value;
-  // console.log(searchValue)
-};
 
-// form.addEventListener('submit', handleSubmit)
+  if (!searchValue) {
+    alert("Please enter a search term");
+    return;
+  }
 
-//RATIO > l-utente digita un nome-per esempio eminem
-
-//2 io quindi metto questo parametro nell-endpoint
-//https://deezerdevs-deezer.p.rapidapi.com/search?q={query}
-
-//3- genero una card per l-artista
-//4- faccio il display di 5 canzoni
-//5- tempo permettendo - faccio vedere album e playlist in basso
-
-const getSearchResults = async () => {
-  const query = form.addEventListener("submit", handleSubmit);
-
-  const url = "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + query;
   const options = {
     method: "GET",
     headers: {
@@ -31,14 +31,51 @@ const getSearchResults = async () => {
   };
 
   try {
-    const response = await fetch(url, options);
+    const response = await fetch(newUrl(searchValue), options);
     const result = await response.json();
+    createTop5SongsResult(result);
+    createArtistCard(result);
     console.log(result);
   } catch (error) {
     console.error(error);
   }
 };
 
-window.addEventListener("DOMContentLoaded", () => {
-  getSearchResults();
-});
+const newUrl = (searchValue) => {
+  const endpoint = "https://deezerdevs-deezer.p.rapidapi.com/search?q=";
+  return `${endpoint}${searchValue}`;
+};
+
+form.addEventListener("submit", handleSubmit);
+
+const createTop5SongsResult = async (obj) => {
+  const ul = document.createElement("ul");
+  ul.classList.add("list-group");
+  top5SongsContainer.appendChild(ul);
+
+  for (let i = 0; i < 5; i++) {
+    const artist = obj.data[i].artist.name;
+    const song_name = obj.data[i].title;
+    const img = obj.data[i].album.cover_medium;
+    const duration = obj.data[i].duration;
+    console.log(artist, song_name, img, duration);
+  }
+};
+
+const createArtistCard = async (obj) => {
+  const name = obj.data[1].artist.name;
+  const picture = obj.data[1].artist.picture_medium;
+  const type = obj.data[1].artist.type;
+
+  artistCardContainer.innerHTML = `<div class="d-flex justify-content-center mt-2 mb-2">
+    <img src="${picture}" class="card-img-top" alt="${name}'s picture" />
+  </div>
+  <div class="card-body text-white p-2">
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">
+    ${type}
+    </p>
+  </div>`;
+};
+
+window.addEventListener("DOMContentLoaded", () => {});
