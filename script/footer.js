a6c64fcba08ac432babfe9930eac559e39e51159
Giovanni <gio.reder@gmail.com>
2024-04-17 22:45:41 +0200

    responsive complete

--- script/footer.js
+++ script/footer.js
@@ -1,13 +1,24 @@
-// START PROGRESS BAR
-const progressBar = document.getElementsByClassName("progress-bar")[0];
-progressInterval = setInterval(() => {
-  const computedStyle = getComputedStyle(progressBar);
-  const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
-  progressBar.style.setProperty("--width", width + 3.4);
-  if (width === 30) {
-    clearInterval(progressInterval);
+/*LINK PLAYBTNFOOTER WITH THE PROGRESSBAR */
+const playBtnFooter = document.getElementById("playBtnFooter");
+playBtnFooter.addEventListener("click", () => {
+  function updateProgressBar() {
+    const progressBar = document.getElementsByClassName("progress-bar")[0];
+    const computedStyle = getComputedStyle(progressBar);
+    const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+    progressBar.style.setProperty("--width", width + 3.4);
+
+    // Controlla se la larghezza raggiunge 30 e ferma l'intervallo in tal caso
+    if (width === 30) {
+      clearInterval(progressInterval);
+    }
   }
-}, 1000);
+
+  // Avvia l'intervallo utilizzando la funzione appena definita
+  const progressInterval = setInterval(updateProgressBar, 1000);
+});
+
+// START PROGRESS BAR
+
 // END PROGRESS BAR
 
 //START SONG TIMER
