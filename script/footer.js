e2499f840cd5312969ced2ac1ab4edaaf99d563c
Giovanni <gio.reder@gmail.com>
2024-04-18 22:13:43 +0200

    link addeventlistener with progressbar e timerfunc

--- script/footer.js
+++ script/footer.js
@@ -1,5 +1,6 @@
 /*LINK PLAYBTNFOOTER WITH THE PROGRESSBAR */
 const playBtnFooter = document.getElementById("playBtnFooter");
+// DENTRO QUEST'ADDEVENTLISTENER HO FATTO PARTIRE LE FUNZIONI DELLA PROGRESSBAR E DEL TIMER DELLE CANZONI
 playBtnFooter.addEventListener("click", () => {
   function updateProgressBar() {
     const progressBar = document.getElementsByClassName("progress-bar")[0];
@@ -7,42 +8,35 @@ playBtnFooter.addEventListener("click", () => {
     const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
     progressBar.style.setProperty("--width", width + 3.4);
 
-    // Controlla se la larghezza raggiunge 30 e ferma l'intervallo in tal caso
     if (width === 30) {
       clearInterval(progressInterval);
     }
   }
 
-  // Avvia l'intervallo utilizzando la funzione appena definita
   const progressInterval = setInterval(updateProgressBar, 1000);
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secondi = seconds % 60;
+    const formattedSeconds = secondi < 10 ? `0${secondi}` : secondi;
+    return `${minutes}:${formattedSeconds}`;
+  }
+
+  function startTimer() {
+    let seconds = 0;
+    timerInterval = setInterval(() => {
+      seconds++;
+      document.getElementById("songTimer").textContent = formatTime(seconds);
+      if (seconds >= 30) {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  }
+  startTimer();
+  // if (seconds > 0 && width > 0) clearInterval(updateProgressBar);
+  // clearInterval(startTimer);
 });
 
-// START PROGRESS BAR
-
-// END PROGRESS BAR
-
-//START SONG TIMER
-
-function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const secondi = seconds % 60;
-  const formattedSeconds = secondi < 10 ? `0${secondi}` : secondi;
-  return `${minutes}:${formattedSeconds}`;
-}
-
-function startTimer() {
-  let seconds = 0;
-  timerInterval = setInterval(() => {
-    seconds++;
-    document.getElementById("songTimer").textContent = formatTime(seconds);
-    if (seconds >= 30) {
-      clearInterval(timerInterval);
-    }
-  }, 1000);
-}
-
 document.addEventListener("DOMContentLoaded", startTimer);
-//END SONG TIMER
 
 const slideValue = document.querySelector("span");
 const inputSlider = document.querySelector("input");
