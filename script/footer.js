7b82bafb9375e286e03613bb67efdb4bbeb33a38
Giovanni <gio.reder@gmail.com>
2024-04-19 10:14:15 +0200

    footer js test

--- script/footer.js
+++ script/footer.js
@@ -2,41 +2,60 @@
 const playBtnFooter = document.getElementById("playBtnFooter");
 // DENTRO QUEST'ADDEVENTLISTENER HO FATTO PARTIRE LE FUNZIONI DELLA PROGRESSBAR E DEL TIMER DELLE CANZONI
 playBtnFooter.addEventListener("click", () => {
-  function updateProgressBar() {
-    const progressBar = document.getElementsByClassName("progress-bar")[0];
-    const computedStyle = getComputedStyle(progressBar);
-    const width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
-    progressBar.style.setProperty("--width", width + 3.4);
+  let isPlayng = true;
 
-    if (width === 30) {
-      clearInterval(progressInterval);
+  const updateProgressBar = function () {
+    if (isPlayng) {
+      const progressBar = document.getElementsByClassName("progress-bar")[0];
+      const computedStyle = getComputedStyle(progressBar);
+      let width = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+      progressBar.style.setProperty("--width", width + 3.4);
+      if (width === 30) {
+        clearInterval(progressInterval);
+      }
     }
-  }
+  };
 
   const progressInterval = setInterval(updateProgressBar, 1000);
-  function formatTime(seconds) {
+  const formatTime = function (seconds) {
     const minutes = Math.floor(seconds / 60);
     const secondi = seconds % 60;
     const formattedSeconds = secondi < 10 ? `0${secondi}` : secondi;
     return `${minutes}:${formattedSeconds}`;
-  }
+  };
 
-  function startTimer() {
+  const startTimer = function () {
     let seconds = 0;
     timerInterval = setInterval(() => {
-      seconds++;
-      document.getElementById("songTimer").textContent = formatTime(seconds);
-      if (seconds >= 30) {
-        clearInterval(timerInterval);
+      if (isPlayng) {
+        seconds++;
+        document.getElementById("songTimer").textContent = formatTime(seconds);
+        if (seconds >= 30) {
+          clearInterval(timerInterval);
+        }
       }
     }, 1000);
-  }
-  startTimer();
-  // if (seconds > 0 && width > 0) clearInterval(updateProgressBar);
-  // clearInterval(startTimer);
-});
+  };
 
-document.addEventListener("DOMContentLoaded", startTimer);
+  startTimer();
+  // QUESTO E QUELLO CHE SUCCEDE AL SECONDO CLICK
+  playBtnFooter.addEventListener("click", () => {
+    isPlayng = !isPlayng;
+    if (!isPlayng) {
+      clearInterval(progressInterval);
+
+      const progressBar = document.getElementsByClassName("progress-bar")[0];
+      const computedStyle = getComputedStyle(progressBar);
+      const currentWidth = parseFloat(computedStyle.getPropertyValue("--width")) || 0;
+      console.log(currentWidth);
+      progressBar.style.setProperty("--width", ` ${currentWidth}`);
+      clearInterval(timerInterval);
+      width = 0;
+    } else {
+      updateProgressBar();
+    }
+  });
+});
 
 const slideValue = document.querySelector("span");
 const inputSlider = document.querySelector("input");
