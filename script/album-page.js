5042618373e8ef90996bb8455dfe22a11c5ec2d5
Luca Nardi <157292118+lucanardi00@users.noreply.github.com>
2024-04-19 10:16:56 +0200

    edited album-page

--- script/album-page.js
+++ script/album-page.js
@@ -80,6 +80,8 @@ const createAlbumHeader = async (obj) =>{
   console.log()
 }
 
+//Creazione Lista Canzoni
+
 const createSongs = async (obj) => {
   const row = document.getElementById("row");
 
@@ -93,20 +95,29 @@ const createSongs = async (obj) => {
     const rank = obj.tracks.data[i].rank;
     const track_num = i + 1;
     const duration = obj.tracks.data[i].duration;
-    const durationInMins = Math.floor(parseInt(duration) / 60)
+    const durationInMins = Math.floor(parseInt(duration) / 60);
     const remainingSeconds = parseInt(duration) % 60;
 
     div.innerHTML = `
     <div id="song-text" class="col d-flex justify-content-between text-white-50 text-end fw-light p-3">
+      <div class="col text-start">${track_num}</div>
+      <div class="col">${artist} - ${song_name}</div>
+      <div class="col">${rank}</div>
+      <div class="col">${durationInMins}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}</div>
+    </div>`;
 
-    <div class="col text-start">${track_num}</div>
-                          <div class="col">${artist} - ${song_name}</div>
-                          <div class="col">${rank}</div>
-                          <div class="col">${durationInMins}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}</div>
-                          </div>
-    `;
+    // Cambia tracknum in button on hover
 
-    console.log(song_name, rank);
+    div.addEventListener('mouseenter', () => {
+      const trackNumDiv = div.querySelector('.col.text-start');
+      trackNumDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="14" fill="white" viewBox="0 0 16 16"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/></svg>`;
+    });
+
+    div.addEventListener('mouseleave', () => {
+      const trackNumDiv = div.querySelector('.col.text-start');
+      trackNumDiv.textContent = track_num;
+    });
+
     row.appendChild(div);
   }
 };
